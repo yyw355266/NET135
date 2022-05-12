7f824059ccfa2967088e8decf66b4f7159420e7c
yyw355266 <3552669895@qq.com>
2022-05-12 15:19:59 +0800

    创建了NEWS控制器

---
+++ demo.UI/Controllers/NewsController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace demo.UI.Controllers
+{
+    public class NewsController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
