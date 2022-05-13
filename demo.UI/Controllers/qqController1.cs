5ea2ea38db93d4675f828df0516465dd857b23a7
ljq3243802882 <3243802882@qq.com>
2022-05-13 16:32:55 +0800

    罗警骑添加了qq控制器

---
+++ demo.UI/Controllers/qqController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace demo.UI.Controllers
+{
+    public class qqController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
