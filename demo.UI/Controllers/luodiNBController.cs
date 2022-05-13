643e78dd98ee60965fcbb286d422e722485b28df
yyw355266 <3552669895@qq.com>
2022-05-13 16:12:13 +0800

    添加了luodiNB控制器

---
+++ demo.UI/Controllers/luodiNBController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace demo.UI.Controllers
+{
+    public class luodiNBController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
