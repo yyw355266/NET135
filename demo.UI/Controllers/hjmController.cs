1713bc57e98033a039a54b56d3471ab676bea546
hjm <2687941339@qq.com>
2022-05-13 16:39:53 +0800

    黄建明添加了一个hjm控制器

---
+++ demo.UI/Controllers/hjmController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace demo.UI.Controllers
+{
+    public class hjmController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
